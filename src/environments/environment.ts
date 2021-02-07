39ccfff86cf941450a03b17e7e3032c762ad5718
HitomaruKonpaku <hunteratthenight@gmail.com>
2021-02-07 12:33:51 +0700

    Add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDjgs_iOEsWZeKU1xMIZLJrubOpjD_bZ_o",
+    authDomain: "kancolleimgbuilder.firebaseapp.com",
+    databaseURL: "https://kancolleimgbuilder-default-rtdb.firebaseio.com",
+    projectId: "kancolleimgbuilder",
+    storageBucket: "kancolleimgbuilder.appspot.com",
+    messagingSenderId: "641922125950",
+    appId: "1:641922125950:web:50d7191a865b605684082f"
+  }
 };
 
 /*
