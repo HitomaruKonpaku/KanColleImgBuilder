fd25e9c6ff59f939c59e3984bae27cc057712593
HitomaruKonpaku <hunteratthenight@gmail.com>
2021-04-03 00:56:18 +0700

    Remove resolver

--- src/app/kancolle/kancolle-routing.module.ts
+++ src/app/kancolle/kancolle-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { KanColleBuilderComponent } from './components/kancolle-builder/kancolle-builder.component'
 import { KanColleComponent } from './components/kancolle.component'
-import { KanColleBuilderConfigResolver } from './resolvers/kancolle-builder-config.resolver'
 
 const routes: Routes = [
   {
@@ -12,7 +11,6 @@ const routes: Routes = [
       {
         path: 'builder',
         component: KanColleBuilderComponent,
-        resolve: { config: KanColleBuilderConfigResolver },
       },
       {
         path: '**',
@@ -24,7 +22,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  providers: [KanColleBuilderConfigResolver],
   exports: [RouterModule],
 })
 export class KanColleRoutingModule { }
