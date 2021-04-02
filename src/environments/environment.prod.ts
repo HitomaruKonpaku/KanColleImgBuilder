0f06cc8d73bc907d5f703a6861c397921dc6bafe
HitomaruKonpaku <hunteratthenight@gmail.com>
2021-04-02 23:54:43 +0700

    Update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyDjgs_iOEsWZeKU1xMIZLJrubOpjD_bZ_o",
-    authDomain: "kancolleimgbuilder.firebaseapp.com",
-    databaseURL: "https://kancolleimgbuilder-default-rtdb.firebaseio.com",
-    projectId: "kancolleimgbuilder",
-    storageBucket: "kancolleimgbuilder.appspot.com",
-    messagingSenderId: "641922125950",
-    appId: "1:641922125950:web:50d7191a865b605684082f"
-  }
-};
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyDjgs_iOEsWZeKU1xMIZLJrubOpjD_bZ_o",
-    authDomain: "kancolleimgbuilder.firebaseapp.com",
-    databaseURL: "https://kancolleimgbuilder-default-rtdb.firebaseio.com",
-    projectId: "kancolleimgbuilder",
-    storageBucket: "kancolleimgbuilder.appspot.com",
-    messagingSenderId: "641922125950",
-    appId: "1:641922125950:web:50d7191a865b605684082f"
-  }
-};
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
