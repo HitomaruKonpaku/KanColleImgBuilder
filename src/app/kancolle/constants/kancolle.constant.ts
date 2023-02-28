a9e6340b72b744cd0c853f7ecf3faa9f28f49b00
HitomaruKonpaku <hunteratthenight@gmail.com>
2023-03-01 03:14:55 +0700

    Use Tibi start2 instead

--- src/app/kancolle/constants/kancolle.constant.ts
+++ src/app/kancolle/constants/kancolle.constant.ts
@@ -2,7 +2,8 @@ export class KanColleConstant {
   public static readonly CONFIG_URL = 'https://kancolleimgbuilder-default-rtdb.firebaseio.com/config.json'
 
   public static readonly GKCOI_GENERATE_OPTIONS = {
-    start2URL: 'https://storage.googleapis.com/kancolleimgbuilder.appspot.com/start2.json',
+    start2URL: 'https://raw.githubusercontent.com/Tibowl/api_start2/master/start2.json',
+    // start2URL: 'https://storage.googleapis.com/kancolleimgbuilder.appspot.com/start2.json',
     shipURL: 'https://storage.googleapis.com/kancolleimgbuilder.appspot.com/ship',
   }
 
